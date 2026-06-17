e8030a73ff1a555d6201f682e542890778847d2c
Maryanne-062 <mgithehu.njeri@gmail.com>
2026-06-17 10:02:30 +0300

    Flask

--- database.py
+++ database.py
@@ -1,98 +1,119 @@
 import psycopg2
 
-#establishing the connection
-conn=psycopg2.connect(host="localhost",database="myduka",user="postgres",password="mng_2006")
+#establishing a connection to a postgres db
+conn = psycopg2.connect(host='localhost',port=5432,user='postgres',password='mng_2006',dbname='myduka')
 
-#creates a cursor object to interact with the database
+#cur object
 cur = conn.cursor()
 
 def get_products():
     cur.execute("select * from products")
-    products_data=cur.fetchall()
+    products_data = cur.fetchall()
     return products_data
+    
 
 def get_sales():
     cur.execute("select * from sales")
-    sales_data=cur.fetchall()
+    sales_data = cur.fetchall()
     return sales_data
 
 def get_stock():
     cur.execute("select * from stock")
-    stock_data=cur.fetchall()
+    stock_data = cur.fetchall()
     return stock_data
 
-def insert_sales(values):
-    cur.execute("insert into sales(pid, quantity) values(%s,%s)", values)
+
+def get_data(table):
+    cur.execute(f"select * from {table}")
+    data = cur.fetchall()
+    return data
+
+
+def insert_products(values):
+    cur.execute(f"insert into products(name,buying_price, selling_price)values{values}")
     conn.commit()
 
-sale1=(1, 2)
-sale2=(2, 1)
-insert_sales(sale1)
-insert_sales(sale2)
+product1 = ('shoes',2000,2500)
+product2 = ('phone',10000,15000)
+
+insert_products(product1)
+insert_products(product2)
+
+
+def insert_products2(values):
+    cur.execute("insert into products(name,buying_price,selling_price)values(%s,%s,%s)", values)
+    conn.commit()
+
+
+def insert_sales(values):
+    cur.execute("insert into sales(pid,quantity)values(%s,%s)", values)
+    conn.commit()
+
 
 def insert_stock(values):
-    cur.execute("insert into stock(pid, stock_quantity) values(%s,%s)", values)
+    cur.execute("insert into stock(pid,stock_quantity)values(%s,%s)", values)
     conn.commit()
 
-stock1=(1, 10)
-stock2=(2, 5)
-insert_stock(stock1)
-insert_stock(stock2)
 
-#cur.execute("insert into products(name, buying_price, selling_price) values('äir fryer',4000,5500)")
-#conn.commit()
-#print(products_data)
+product3 = ('laptop',40000,50000)
+insert_products2(product3)
 
-def insert_product(values):
-    #cur.execute(f"insert into products(name, buying_price, selling_price) values{values}")
-    cur.execute("insert into products(name, buying_price, selling_price) values(%s,%s,%s)",values)
-    conn.commit()
 
-product1=("LG TV", 40000, 50000)
-product2=("Samsung TV", 35000, 45000)
-
-insert_product(product1)
-insert_product(product2)
-
-# User input to add a new product
 def sales_per_day():
     cur.execute("""
-    SELECT DATE(sales.created_at) AS date, 
-           SUM(sales.quantity * products.buying_price) AS total_sales 
-    FROM sales 
-    JOIN products ON products.id = sales.pid 
-    GROUP BY date;
-""")
+      select date(sales.created_at) as date, sum(sales.quantity * products.selling_price) as
+      total_sales from sales join products on products.id = sales.pid  group by date;
+    """)
     daily_sales = cur.fetchall()
     return daily_sales
 
+
 def profit_per_day():
     cur.execute("""
-    SELECT DATE(sales.created_at) AS date, 
-           SUM(sales.quantity * (products.selling_price - products.buying_price)) AS total_profit 
-    FROM products
-    JOIN sales ON sales.pid = products.id 
-    GROUP BY date;
-""")
+        select date(sales.created_at) as date, sum(sales.quantity *( products.selling_price -
+        products.buying_price)) as total_sales from sales join products on products.id = sales.pid
+         group by date;
+    
+    """)
     daily_profit = cur.fetchall()
     return daily_profit
 
+
+
 def sales_per_product():
     cur.execute("""
-    select products.name as p_name ,
-        sum(sales.quantity * products.selling_price) as total_sales 
-        from products join sales on sales.pid = products.id 
-        group by p_name;
-""")
+        select products.name as p_name , sum(sales.quantity * products.selling_price)  as total_sales
+        from products join sales on sales.pid = products.id group by p_name;
+    """)
     product_sales = cur.fetchall()
     return product_sales
 
+
 def profit_per_product():
     cur.execute("""
-    select products.name as p_name ,
-    sum(sales.quantity *( products.selling_price - products.buying_price)) as total_sales
-    from products join sales on sales.pid = products.id 
-    group by p_name;
-""")
+        select products.name as p_name , sum(sales.quantity *( products.selling_price - 
+        products.buying_price))  as total_sales from products join sales on sales.pid = products.id group by p_name;
+    """)
     product_profit = cur.fetchall()
     return product_profit
+
+
+def check_available_stock(pid):
+    cur.execute("select sum(stock.stock_quantity) from stock where pid = %s",(pid,))
+    total_stock = cur.fetchone()[0] or 0
+
+    cur.execute("select sum(sales.quantity) from sales where pid = %s",(pid,))
+    total_sold = cur.fetchone()[0] or 0
+
+    return total_stock - total_sold
+
+
+def check_user_exists(email):
+    cur.execute("select * from user where email = %s",(email,))
+    user = cur.fetchone()
+    return user
+
+
+def create_user(user_details):
+    cur.execute("insert into users(full_name,email,phone_number,password)values(%s,%s,%s,%s)",user_details)
+    conn.commit()
--- main.py
+++ main.py
@@ -1,22 +1,65 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect,url_for,flash
+from database import get_products,get_sales,get_stock,insert_products,check_available_stock,insert_sales,check_user_exists,create_user
+from flask_bcrypt import Bcrypt
+
 
 app= Flask(__name__)
 
+bcrypt = Bcrypt(app)
+
+app.secret_key= '78cbn88enhcucbhd99njsnns'
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    name="Jack"
+    return render_template("index.html", name=name)
 
 @app.route('/products')
 def products():
-    return render_template("products.html")
+    products_data=get_products()
+    return render_template("products.html",products_data=products_data)
+
+@app.route('/add_products',methods=['GET','POST'])
+def add_products():
+    if request.method == 'POST':
+        product_name = request.form['p_name']
+        buying_price = request.form['b_price']
+        selling_price = request.form['s_price']
+
+        new_product = (product_name,buying_price,selling_price)
+        insert_products(new_product)
+        flash("Product added successfully",'success') 
+
+    return redirect(url_for('products'))
 
 @app.route('/sales')
 def sales():
-    return render_template("sales.html")
+    sales=get_sales()
+    products=get_products()
+    return render_template("sales.html",sales=sales,products=products)
+
+@app.route('/make_sale',methods=['GET','POST'])
+def make_sale():
+    if request.method == 'POST':
+        pid = request.form['pid']
+        quantity = request.form['quantity']
+
+        new_sale = (pid,quantity)
+        available_stock = check_available_stock(pid)
+
+        if available_stock < float(quantity):
+            flash("Insufficient stock,add more",'danger')
+            return redirect(url_for('sales'))
+
+        insert_sales(new_sale)
+        flash("Sale added successfully",'success')
+    return redirect(url_for('sales'))
 
 @app.route('/stock')
 def stock():
-    return render_template("stock.html")
+    stock=get_stock
+    products=get_products
+    return render_template("stock.html",stock=stock,products=products)
 
 @app.route('/dashboard')
 def dashboard():
@@ -24,6 +67,22 @@ def dashboard():
 
 @app.route('/register')
 def register():
+    if request.method == 'POST':
+        full_name = request.form['full_name']
+        email = request.form['email']
+        phone_number = request.form['phone']
+        password = request.form['password']
+
+        existing_user = check_user_exists(email)
+        if not existing_user:
+            hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+            new_user = (full_name,email,phone_number,hashed_password)
+            create_user(new_user)
+            flash("User created successfully",'success')
+            return redirect(url_for('login'))
+        else:
+            flash("User already exists,please login instead",'danger')
+
     return render_template("register.html")
 
 @app.route('/login')
